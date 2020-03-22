427617a46f08d571a9699bccab20b54c4c9b8b95
Cleber Gomes Motta <cgmbnu@gmail.com>
2020-03-22 07:09:56 -0300

    adicioando funções para capturar os valores digitados

--- crud/src/views/produtos/cadastro.js
+++ crud/src/views/produtos/cadastro.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 
 class CadastroProduto extends React.Component {
+    state ={
+        nome:'',
+        sku:'',
+        descricao:'',
+        preco:0,
+        fornecedor:''
+    }
+
+    onChange =(event) =>{
+       const valor = event.target.value
+       const nomeDoCampo = event.target.name
+       this.setState({[nomeDoCampo] : valor})
+    }
+
+   onSubmit =() =>{
+       console.log(this.state)
+   }
+
     render() {
         return (
             <div className="card">
@@ -13,14 +31,22 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" className="form-control"></input>
+                                <input type="text" 
+                                name="nome" 
+                                onChange={this.onChange}
+                                value={this.state.nome} 
+                                className="form-control"></input>
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>SKU: *</label>
-                                <input type="text" className="form-control"></input>
+                                <input type="text" 
+                                name="sku" 
+                                onChange={this.onChange}
+                                value={this.state.sku} 
+                                className="form-control"></input>
                             </div>
                         </div>
                     </div>
@@ -29,7 +55,10 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Descrição:</label>
-                                <textarea className="form-control" />
+                                <textarea name="descricao" 
+                                onChange={this.onChange}
+                                value={this.state.descricao} 
+                                className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -38,20 +67,28 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Preço: *</label>
-                                <input type="text" className="form-control"></input>
+                                <input name="preco" 
+                                onChange={this.onChange}
+                                type="text" 
+                                value={this.state.preco} 
+                                className="form-control"></input>
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Fornecedor: *</label>
-                                <input type="text" className="form-control"></input>
+                                <input name="fornecedor" 
+                                type="text" 
+                                onChange={this.onChange}
+                                value={this.state.fornecedor} 
+                                className="form-control"></input>
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-1">
-                            <button className="btn btn-outline-success">Salvar</button>
+                            <button onClick={this.onSubmit} className="btn btn-outline-success">Salvar</button>
                         </div>
                         <div className="col-md-1">
                             <button className="btn btn-outline-primary">Limpar</button>
